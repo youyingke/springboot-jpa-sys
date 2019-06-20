6698355348e7d1df4258b34c7570dc0a4fa27bf2
253524877 <253524877@qq.com>
2019-06-20 09:53:51 +0800

    添加Schedule定时任务代码

--- src/main/java/com/hawk/demo/SpringbootDataJpaSysApplication.java
+++ src/main/java/com/hawk/demo/SpringbootDataJpaSysApplication.java
@@ -2,8 +2,13 @@ package com.hawk.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+/**
+ * 如果需要执行定时任务需要加@EnableScheduling注解
+ */
 @SpringBootApplication
+@EnableScheduling
 public class SpringbootDataJpaSysApplication {
 
 	public static void main(String[] args) {
